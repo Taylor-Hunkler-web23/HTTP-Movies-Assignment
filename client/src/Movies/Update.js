e25487bada3c7b0cd6458d033ba8fdf2d11b851d
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-17 15:39:15 -0400

    Built basic form for update

--- client/src/Movies/Update.js
+++ client/src/Movies/Update.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+
+const initialMovie ={
+    id: '',
+    title: '',
+    director: '',
+    metascore: '',
+    stars: [],
+  }
+  
 
 const UpdateForm = props => {
+    const [movie, setMovie] = useState(initialMovie);
 
+
+    const changeHandler = event => {
+       setMovie({
+           ...movie,
+           [event.target.name]: event.target.value
+       })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+    }
 
     return(
         <form>
+           <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          placeholder="Title"
+          value={movie.title}
+        />
+         <input
+          type="text"
+          name="director"   
+          onChange={changeHandler}
+          placeholder="Director"
+          value={movie.director}
+        />
+         <input
+          type="text"
+          name="metascore"
+          onChange={changeHandler}
+          placeholder="Metascore"
+          value={movie.metascore}
+        />
+         <input
+          type="text"
+          name="stars"
+          onChange={changeHandler}
+          placeholder="Stars"
+          value={movie.stars}
+        />
 
         </form>
     )
